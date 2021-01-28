b34049201720fbe5d1aac57c55895362e846e135
dariovp <dariovp01@gmail.com>
2021-01-28 15:48:59 -0300

    ???

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,11 +1,22 @@
-import { StyledNavBar } from './NavBar.styles';
+import { NavBarItem, NavBarList, StyledNavBar } from './NavBar.styles';
 
 
-export default function NavBar ({ children }) {
+export default function NavBar () {
 
 	return (
 		<StyledNavBar className="navbar navbar-expand-lg navbar-dark bg-dark">
-			{ children }			
+			<NavBarList>
+				<NavBarItem>
+					<button className="btn btn-light shadow-none">
+						asdasd
+					</button>
+				</NavBarItem>
+				<NavBarItem>
+					<button className="btn btn-light">
+						asdasd
+					</button>
+				</NavBarItem>
+			</NavBarList>			
 		</StyledNavBar>
 	);
 }
--- components/NavBar.styles.js
+++ components/NavBar.styles.js
@@ -4,9 +4,17 @@ import { colors } from '../styles/theme';
 export const StyledNavBar = styled.nav`
 	width: 100%;
 	height: 10vh;
-	padding: 2rem;
+	padding: 0rem 1rem;
 	background-color: ${colors.primary};
-	display: flex;
-	justify-content: flex-start;
-	align-items: center;
+`;
+
+export const NavBarList = styled.ul`
+	list-style-type: none;
+	margin: 0;
+	padding: 0;
+`;
+
+export const NavBarItem = styled.li`
+	margin: 0rem 0.3rem;
+	display: inline;
 `;
