54f23e37b4edb73fa3429cd71fc8bddcdcbbc7a6
aenoris <aenoriss@gmail.com>
2021-01-28 23:03:52 -0300

    intro v1.0

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,9 @@ import Head from 'next/head'
 import NavBar from '../components/NavBar'
 import { Section } from '../components/Section'
 import styles from '../styles/Home.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFacebook,faInstagram,faLinkedin,faTwitter} from '@fortawesome/free-brands-svg-icons'
+
 
 export default function Home() {
   return (
@@ -13,12 +16,31 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Moony</a>
-        </h1>
 
         <Section className="container-fluid">
+          <div className={styles.intro}>
+            <div className={styles.description}>
+              <div>
+                <h1 className={styles.title}>
+                  Welcome to <label className = {styles.moonyTitle}>Moony</label>
+                </h1 >
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                </p>
+                <div className = {styles.socialMediaIcons}>
+                  <FontAwesomeIcon icon={faFacebook} className={styles.icon} />
+                  <FontAwesomeIcon icon={faInstagram} className={styles.icon}/>
+                  <FontAwesomeIcon icon={faLinkedin} className={styles.icon}/>
+                  <FontAwesomeIcon icon={faTwitter} className={styles.icon}/>
 
+                </div>
+              </div>
+            </div>
+
+            <div className={styles.rocketLogo}>
+              🚀
+          </div>
+          </div>
         </Section>
 
       </main>
