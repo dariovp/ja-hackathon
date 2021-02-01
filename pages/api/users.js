055c0567339485f5f0aeef18c4ba325d2f0fafc0
dariovp <dariovp01@gmail.com>
2021-02-01 16:39:13 -0300

    ando

--- pages/api/users.js
+++ pages/api/users.js
@@ -1,25 +1,22 @@
-// import pgp from 'pg-promise';
-
-// pgp({
-//     noWarnings: true
-// })
-
-// const db = pgp(`postgres://postgres:12345678@localhost:5432/user-test`);
+const pgp = require('pg-promise')({
+    noWarnings: true
+})
+const db = pgp(`postgres://postgres:12345678@localhost:5432/moonlanding`);
 
 module.exports = (req, res) => {
     try {
 		const { name, email } = req.query
 		console.log(req.query);
 
-		res.status(200).send({name: "asdasd", email: "asdasdfsdsf"})
+		// res.status(200).send({name: "asdasd", email: "asdasdfsdsf"})
 
-        // if(!name || !email){
-        //     return res.status(422).send({error: ['Missing one or more fields']})
-        // }
+        if(!name || !email){
+            return res.status(422).send({error: ['Missing one or more fields']})
+        }
 
-        // const user = await db.one('INSERT INTO User(name, email) VALUES($1, $2) RETURNING *', [name, email])
+        const user = await db.one('INSERT INTO User(name, email) VALUES($1, $2) RETURNING *', [name, email])
 
-        // res.status(200).json(user)
+        res.status(200).json(user)
 
     } catch (error) {
         // console.error(error);
