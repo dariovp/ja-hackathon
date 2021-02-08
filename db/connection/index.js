8eac7cb6d54c753d47a50d0809bac9de322d4322
MatiasRetondo <matiasezeretondo@gmail.com>
2021-02-08 15:43:17 -0300

    connection

--- db/connection/index.js
+++ db/connection/index.js
@@ -5,6 +5,25 @@ const { Sequelize } = require('sequelize');
 const env ='development';
 const config = require(__dirname + '/../config/config.js')[env];
 
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
+
 
 
 
