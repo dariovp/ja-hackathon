6cd9df9dc50f90d611040c8b07e77a489792fffe
MatiasRetondo <matiasezeretondo@gmail.com>
2021-02-08 16:26:02 -0300

    change confin

--- db/config/config.js
+++ db/config/config.js
@@ -17,13 +17,11 @@ module.exports = {
     logging: false,
   },
   production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    dialect: 'postgres',
-    dialectOptions: {
-      ssl: true,
-    },
+    username: "hylsbtewohsbvf",
+    password: "9f9c913865d773ea6d717abbf7f65444ee4739ef150dddba6ef292f18acb7757",
+    database: "d3bvigi4121ji3",
+    host: "ec2-54-162-119-125.compute-1.amazonaws.com",
+    dialect: "postgres",
+    port :  5432,
   },
 };
