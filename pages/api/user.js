7b721aa7c18f1d8fb14fb0f798dbcbe6f7bd61ed
MatiasRetondo <matiasezeretondo@gmail.com>
2021-02-09 17:56:36 -0300

    chadelalora

--- pages/api/user.js
+++ pages/api/user.js
@@ -27,11 +27,7 @@ export default async (req, res) => {
 		// res.status(200).send({name: "asdasd", email: "asdasdfsdsf"})
 
 		if(!email || !mentor){
-			let test = db.User.findAll();
-
-			return res.send(test)
-
-			//return res.status(422).send({error: 'Missing one or more fields'})
+			return res.status(422).send({error: 'Missing one or more fields'})
 		}
 
 		if(email.match(/(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))/)) {
@@ -91,7 +87,7 @@ export default async (req, res) => {
 
 				// ....
 
-
+				console.log("aberrrrrrrrrrr", user)
 				res.status(200).json(user)
 			}
 
