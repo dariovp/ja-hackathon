74667de0f4cab494ddcadf96ae60aa118e44ab46
aenoris <aenoriss@gmail.com>
2021-02-10 02:03:11 -0300

    asdada

--- components/Section/Intro/index.jsx
+++ components/Section/Intro/index.jsx
@@ -3,7 +3,8 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFacebook, faInstagram, faLinkedin, faTwitter } from '@fortawesome/free-brands-svg-icons';
 import { useState } from "react";
 import { Button, Collapse } from "react-bootstrap";
-
+import Image from 'next/image';
+import smtpImage from "../../../public/smartphone.png"
 
 export default function Intro(props) {
     let duration = props.duration;
@@ -20,18 +21,17 @@ export default function Intro(props) {
                     <p className={styles.subtitle}>
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                     </p>
-                    <div> 
-                        <form>
-                            <div className={`form-group ${styles.registerInput}`}>
-                                    <input className={`form-control ${styles.registerInput}`} type="email" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
-                                <div><button type="submit" className={`btn btn-primary ${styles.inputButton}`} >Join</button></div>
-                            </div>
-                        </form>
+                    <div className ={styles.registerStyle}> 
+                        <h1>Get Onboard</h1>
+                        <div className={`form-group ${styles.registerInput}`}>
+                            <input className={`form-control`} type="email" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" onInput={(e) => setEmail(e.target.value)} />
+                            <button type="submit" className={`btn btn-primary ${styles.inputButton}`} onClick={(e) => submitRegister()}>Join</button>
+                        </div>
                     </div>
                 </div>
             </div>
 
-            <div className={styles.rocketSection}>
+            {/* <div className={styles.rocketSection}>
                 <div className={styles.rocketLogo}>🚀</div>
                 {duration && <div className={styles.counter}>
                     <div><div className={styles.timebox}>{duration['_data']['days']}</div><span className={styles.dateInfo}>Days</span></div>
@@ -39,10 +39,18 @@ export default function Intro(props) {
                     <div><div className={styles.timebox}>{duration['_data']['minutes']}</div><span className={styles.dateInfo}>Minutes</span></div>
                     <div><div className={styles.timebox}>{duration['_data']['seconds']}</div><span className={styles.dateInfo}>Seconds</span></div>
                 </div>}
-                <div>
-                    <div></div>
-                </div>
+            </div> */}
+
+            <div>
+                <Image
+                className={styles.imgStyle}
+                src={smtpImage}
+                alt="Picture of the author"
+                width={300}
+                height={520}/>            
+            
             </div>
+
         </div>
 
     )
