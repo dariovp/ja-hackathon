cd6bafe611cc848da0cd42d096178d6997daa762
MatiasRetondo <matiasezeretondo@gmail.com>
2021-02-12 20:22:56 -0300

    cors

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 const withImages = require('next-images')
 
 
-module.exports = withImages({})
+module.exports = withImages({
+    async rewrites() {
+        return [{
+            source: '/api/:path*',
+            destination: 'https://www.moonyapp.site/:path*'
+        }]
+    }
+})
