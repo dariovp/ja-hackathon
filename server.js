2681ecc9805016f6c5ee67f1675ea8795a821e1d
dariovp <dariovp01@gmail.com>
2021-02-17 18:53:52 -0300

    no crashees plis

--- server.js
+++ server.js
@@ -5,16 +5,19 @@ const next = require('next');
 const port = process.env.PORT || 3000;
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
+const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
 	const server = express();
 
 	server.all('*', (req, res) => {
-		console.log("req ", req.headers)
-		if (req.headers['x-forwarded-proto'] != 'https') {
+		if (req.headers['x-forwarded-proto']!='https') {
 			console.log("no tiene http")
-			res.redirect('https://www.moonyapp.site' + req.url)
+			res.redirect('https://moonyapp.site' + req.url)
+		} else {
+			return handle(req, res);
 		}
+		
 	})
 
 
