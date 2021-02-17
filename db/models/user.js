f239855577dc9547a9876ab5af0c109c43a55c72
dariovp <dariovp01@gmail.com>
2021-02-17 17:40:12 -0300

    aa

--- db/models/user.js
+++ db/models/user.js
@@ -8,7 +8,7 @@ const User = sequelize.define(
 	'User',
 	{
 		id: {
-			type: DataTypes.INTEGER, // Tipo de dato.
+			type: Sequelize.INTEGER, // Tipo de dato.
 			primaryKey: true, // Primary Key set.
 			allowNull: false, // No nulleable.
 			autoIncrement: true
