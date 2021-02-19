5d87ff912b0ab752741d02aa0375846066558296
MatiasRetondo <matiasezeretondo@gmail.com>
2021-02-19 18:58:19 -0300

    change name

--- pages/api/user.js
+++ pages/api/user.js
@@ -33,7 +33,7 @@ export default async (req, res) => {
 		let name = req.body.firstName
 
 
-		if (email && name == undefined && email.match(/(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))/)){
+		if (email && req.body.name == undefined && email.match(/(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))/)){
 			let emailCheck = await db.User.findOne({
 				where: {
 					email: req.body.email,
