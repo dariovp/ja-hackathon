1be64e47c5ec8cdf9711af565eb8dfe57698f16e
aenoris <aenoriss@gmail.com>
2021-02-23 03:55:01 -0300

    asdadasd

--- components/Section/Intro/index.jsx
+++ components/Section/Intro/index.jsx
@@ -4,6 +4,8 @@ import { Col, Container, Row } from "react-bootstrap";
 import Image from 'next/image';
 import draw from "../../../public/rocket.svg"
 import rotbtc from "../../../public/tenor.gif"
+import networkIcon from "../../../public/networkIcon.png"
+import rocketIcon from "../../../public/rocketIcon.png"
 import mailchimp from "../../../pages/api/mailchimp"
 import axios from "axios";
 import { useRouter } from "next/router"
@@ -13,7 +15,7 @@ export default function Intro(props) {
 	const [email, setEmail] = useState('');
 	const [name, setName] = useState('');
 	const [open, setOpen] = useState(false);
-	const [regSwitch, setregSwitch] = useState(0);
+	const [regSwitch, setregSwitch] = useState(2);
 	const [user, setUser] = useState({});
 
 	const router = useRouter();
@@ -30,9 +32,23 @@ export default function Intro(props) {
 	// 	})
 	// },[]);
 
+	function copyText() {
+		/* Get the text field */
+		var copyText = document.getElementById("referralUrl");
+	  
+		/* Select the text field */
+		copyText.select();
+		copyText.setSelectionRange(0, 99999); /* For mobile devices */
+	  
+		/* Copy the text inside the text field */
+		document.execCommand("copy");
+	  
+		/* Alert the copied text */
+	  }
+
 	function checkReg() {
-		axios.post(`../../../api/user`, {email}).then(response => {
-			if(response.status != 204){
+		axios.post(`../../../api/user`, { email }).then(response => {
+			if (response.status != 204) {
 				setUser(response["data"]) //["dataValues"]
 				setregSwitch(3)
 			} else {
@@ -49,19 +65,19 @@ export default function Intro(props) {
 		}).then(response => {
 			console.log("RESPONSE MAILCHIMP", response)
 			axios.post(`../../../api/user`, { email, name, rc })
-			.then(response => {
-				console.log("RESPONSE DB", response["data"]);
-				setEmail("");
-				setregSwitch(3);
-				setUser({
-					id: response["data"]["id"],
-					name: response["data"]["firstName"],
-					points: response["data"]["points"],
-					ref: response["data"]["ref"],
+				.then(response => {
+					console.log("RESPONSE DB", response["data"]);
+					setEmail("");
+					setregSwitch(3);
+					setUser({
+						id: response["data"]["id"],
+						name: response["data"]["firstName"],
+						points: response["data"]["points"],
+						ref: response["data"]["ref"],
+					})
+				}).catch(response => {
+					console.log("ERROR DB", response)
 				})
-			}).catch(response => {
-				console.log("ERROR DB", response)
-			})
 		}).catch(error => {
 			console.log("ERROR MAILCHIMP", error)
 		})
@@ -69,46 +85,69 @@ export default function Intro(props) {
 
 	return (
 		<Container fluid className={`${styles.intro}`}>
-			<Row lg={2} md={2} sm={2} xs={1} className="py-2 justify-content-md-center align-items-md-center w-100 h-100">
+			<Row xl={4} lg={4} md={2} sm={2} xs={1} className="py-2 justify-content-md-center align-items-md-center">
+				<Col xl={1}>
+				</Col>
 				<Col xl={4} >
 					<div className={styles.description}>
 						<h1 className={styles.title, styles.delirio}>
 							Bienvenido a <label className={styles.moonyTitle}>Moony</label>
 						</h1>
 						<p className={styles.subtitle}>
-							Somos tu red social de inversiones. 
-							Estás empezando? Seguí y aprendé de los que saben.
+							Somos tu red social de inversiones
+							¿Estás empezando? Seguí y aprendé de los que saben.
 						</p>
-						{regSwitch == 0 && <button type="button" className={`btn btn-primary ${styles.regButton}`} onClick = {() => setregSwitch(1)}>Unite</button>}
+						{regSwitch == 0 && <button type="button" className={`btn btn-primary ${styles.regButton}`} onClick={() => setregSwitch(1)}>Unite</button>}
 
 						{regSwitch != 0 && <div className={styles.registerStyle}>
 							{(regSwitch == 1 || regSwitch == 2) && <div>
+
 								<h2 className={styles.regTitle}>Sumate <img className={styles.rotbtc} src={rotbtc} width="40px"></img></h2>
 								<p>Queremos que seas parte de la experiencia Moony, dejanos tus datos y se parte del pre-release.</p>
+
 								{regSwitch == 1 && <div className={`form-group ${styles.registerInput}`}>
-									<input className={`form-control ${styles.inputForm}` } type="email" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Ingresa tu email" onInput={(e) => setEmail(e.target.value)} />
+									<input className={`form-control ${styles.inputForm}`} type="email" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Ingresa tu email" onInput={(e) => setEmail(e.target.value)} />
 									<button type="submit" className={`btn btn-primary ${styles.inputButton}`} onClick={(e) => checkReg(e)}>Siguiente</button>
 								</div>}
 
 								{regSwitch == 2 && <div className={`form-group ${styles.registerInput}`}>
-									<input className={`form-control`} type="text" id="exampleInputName1" aria-describedby="nameHelp" placeholder="Enter name" onInput={(e) => setName(e.target.value)} />
+									<input className={`form-control ${styles.inputForm}`} type="text" id="exampleInputName1" aria-describedby="nameHelp" placeholder="Ingresa tu nombre" onInput={(e) => setName(e.target.value)} />
 									<button type="submit" className={`btn btn-primary ${styles.inputButton}`} onClick={(e) => submitRegister(e)}>Despegar</button>
 								</div>}
 							</div>}
 
-							
-							{regSwitch == 3 && <div className={`form-group ${styles.registerStyle}`}>
-								<h1>Bienvenido {user.name}</h1>
+							{regSwitch == 3 && <div>
+								<div className={styles.refHeader}>
+									<h2>Hola <label className={styles.refName}>{user.name}</label></h2>
+									<h4 className={styles.regTitle}>¡Referi y gana!</h4>
+								</div>
 								<div >
 									<Row className={styles.registerRow}>
-										<div className={styles.referralCode}><p>Referal Link: https://www.moonyapp.site/?rc={user.id}</p></div>
+										<div className="input-group mb-3">
+											<input type="text" id="referralUrl" className={`form-control ${styles.inputForm}`} placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="basic-addon2" value={`https://www.moonyapp.site/?rc=${user.id}`} />
+											<div className="input-group-append">
+												<label className={`input-group-text ${styles.inputForm} ${styles.shareStyle}`} onClick={() => copyText()} >Copiar</label>
+											</div>
+										</div>
 									</Row>
 									<Row className={styles.registerRow}>
 										<Col className={styles.registerRow}>
-											<div className={styles.registerBox}>Referidos {user.ref} </div>
+											<div className={styles.registerBox}>
+												<div><img src={networkIcon} width="50px"></img></div>
+												<div className = {styles.registerBoxCol}>
+													<div className={styles.refTitle}>Referidos</div>
+													<div className={styles.refNum}>{user.ref}</div> 
+												</div>
+											</div>
 										</Col>
 										<Col className={styles.registerRow}>
-											<div className={styles.registerBox}>Coins {user.points}</div>
+											<div className={styles.registerBox}>
+												<div><img src={rocketIcon} width="50px"></img></div>
+												<div className = {styles.registerBoxCol}>
+													<div className={styles.refTitle}>Moony´s</div>
+													<div className={styles.refNum}>{user.coins}</div> 
+												</div>
+											</div>
 										</Col>
 									</Row>
 								</div>
@@ -116,15 +155,16 @@ export default function Intro(props) {
 						</div>}
 					</div>
 				</Col>
-				<Col xl={6} className={`d-flex align-items-center justify-content-center`}>
+				<Col xl={7} className={`d-flex align-items-end justify-content-center`}>
 					<Image
 						src={draw}
 						alt="Picture of the author"
-						width={500}
+						width={700}
 						height={600} />
-				</Col>				
+				</Col>
 			</Row>
 		</Container>
-	)}
+	)
+}
 
 
