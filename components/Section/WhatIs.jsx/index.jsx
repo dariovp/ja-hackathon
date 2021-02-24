a08c533fd2a3b55241497c61209ce2c9e464cd28
dariovp <dariovp01@gmail.com>
2021-02-24 17:09:41 -0300

    AHORA SI?

--- components/Section/WhatIs.jsx/index.jsx
+++ components/Section/WhatIs.jsx/index.jsx
@@ -1,5 +1,8 @@
 import styles from '../WhatIs.jsx/WhatIs.module.css'
 import Card from "../../Commons/card"
+import smtpImage from "../../../public/smartphone3.gif"
+
+import Image from 'next/image';
 import { motion } from "framer-motion"
 
 
@@ -9,7 +12,8 @@ import { Col, Container, Row } from 'react-bootstrap';
 
 export default function WhatIs () {
 	const cardText1 = "Copiá los movimientos de los mejores inversores y ganá como ellos mientras aprendés en el camino"
-	const cardText2 = "Descubrí y conectá con gente de la comunidad. Enterate cuáles son las opiniones y últimos hallazgos de los expertos en el mercado. "
+	const cardText2 = "Descubrí y conectá con gente de la comunidad"
+	// Enterate cuáles son las opiniones y últimos hallazgos de los expertos en el mercado. 
 	const cardText3 = "Si sos trader, compartí y monetiza tus conocimientos y opiniones!"
 
 	const variants = {
@@ -25,15 +29,21 @@ export default function WhatIs () {
 					<h1>Discover</h1>
 					<p className={styles.description}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi</p>
 				</div>
-				<Row xl={3} lg={3} md={3} sm={1} xs={1}>
-					<Col className="d-flex justify-content-center align-items-center my-2">
-						<Card title = "Feature 1" description={cardText1} icon = {faChartLine} />
+				<Row className="d-flex justify-content-center align-items-center">
+					<Col xl={6} lg={6} md={6}>
+						<div className=" w-auto p-1 d-flex justify-content-center align-items-center">
+							<Image
+								className={styles.imgStyle}
+								src={smtpImage}
+								alt="Picture of the author"
+								width={260}
+								height={520} />
+						</div>
 					</Col>
-					<Col className="d-flex justify-content-center align-items-center my-2">
-						<Card title = "Feature 2" description={cardText2} icon = {faCommentsDollar}/>
-					</Col>
-					<Col className="d-flex justify-content-center align-items-center my-2">
-						<Card title = "Feature 3" description={cardText3} icon = {faUser}/>
+					<Col xl={6} lg={6} md={6} className="d-flex justify-content-center align-items-center flex-column my-2">
+						<Card description={cardText1} icon = {faChartLine} />
+						<Card description={cardText2} icon = {faCommentsDollar}/>
+						<Card description={cardText3} icon = {faUser}/>
 					</Col>
 				</Row>
 			</Container>
